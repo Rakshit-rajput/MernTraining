ca6c1b37d240d130905a4d1763a9e9a4d22d9e88
Rakshit rajput <Rakshitrajput212@gmail.com>
2024-04-05 16:33:27 +0530

    Done Day4

--- Day4/formHandling/script.js
+++ Day4/formHandling/script.js
@@ -1,3 +1,21 @@
-const firstNameChange = (e) => {
-  console.log(e);
-};
+function submitForm(e) {
+  e.preventDefault();
+
+  const res = {
+    hobbies: [],
+  };
+  const t = e.target;
+  for (let i = 0; i < t.length; i++) {
+    const ty = t[i].type;
+    const vl = t[i].value;
+    const nm = t[i].name;
+    if (ty !== "submit") {
+      res[nm] = vl;
+    }
+
+    if (ty === "checkbox" && t[i].checked) {
+      res.hobbies.push(vl);
+    }
+  }
+  console.log(res);
+}
